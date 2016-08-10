0de5e9b31a0d3fc29e043e6c257ac547aad71cda
zhaoyang.liu <zhaoyang.liu@tsj-001577.ytx.com>
2016-08-10 15:51:38 +0800

    my first push

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: {
+    app: path.resolve(APP_PATH, 'index.jsx')
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  //enable dev source map
+  devtool: 'eval-source-map',
+  //enable dev server
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, loader: 'babel', include: APP_PATH, query: {presets: ['es2015', 'react']}},
+      {test: /\.css$/, loader: 'style!css'}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My first react app'
+    })
+  ]
+}
